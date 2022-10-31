c29b57739a8c56411ba7779163612c60cb03f09a
giovanniPepi <gtpepi@proton.me>
2022-10-31 10:57:53 -0300

    update files

--- express-locallibrary/app.js
+++ express-locallibrary/app.js
@@ -9,14 +9,6 @@ var usersRouter = require("./routes/users");
 
 var app = express();
 
-// mongoose connection
-const mongoose = require("mongoose");
-const mongoDB =
-  "mongodb+srv://locallibrary:iyO2yB97VThT84xC@expresslocallibrary.46fkx7d.mongodb.net/?retryWrites=true&w=majority";
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error"));
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
