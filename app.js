828ed21cb12788c3a780abc106ef15f3abc1d9af
giovanniPepi <gtpepi@proton.me>
2022-12-12 16:12:11 -0300

    Update pkgs

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog");
 
 var app = express();
 dotenv.config();
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -47,5 +49,8 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render("error");
 });
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
